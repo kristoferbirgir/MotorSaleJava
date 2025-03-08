4e522e77807ff1f071a5a14b3936e7794be92a77
janusbirgisson <janusbirgisson@gmail.com>
2025-03-08 17:31:47 +0000

    Add refresh listings on home screen with swipe down

--- app/src/main/java/is/hbv601g/motorsale/ListingsFragment.java
+++ app/src/main/java/is/hbv601g/motorsale/ListingsFragment.java
@@ -1,6 +1,7 @@
 package is.hbv601g.motorsale;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import java.util.List;
 
 import is.hbv601g.motorsale.adapters.VehicleAdapter;
@@ -24,9 +26,12 @@ public class ListingsFragment extends Fragment {
     private ListingService listingsService;
     private VehicleAdapter adapter;
 
+    private SwipeRefreshLayout swipeRefreshLayout;
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentListingsBinding.inflate(inflater, container, false);
+        swipeRefreshLayout = binding.swipeRefreshLayout;
         return binding.getRoot();
     }
 
@@ -39,15 +44,26 @@ public class ListingsFragment extends Fragment {
         // Setup RecyclerView
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        // Fetch listings
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            fetchListings();
+        });
+
+        // Fetch listingsThe
         fetchListings();
     }
 
+
     private void fetchListings() {
         listingsService.findAll(listings -> {
-            if (listings == null || listings.isEmpty()) {
+            if (swipeRefreshLayout.isRefreshing()) {
+                adapter.updateListings(listings);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            if (listings.isEmpty()) {
                 Toast.makeText(getContext(), "No listings found", Toast.LENGTH_SHORT).show();
             } else {
+                Log.e("ListingsFragment", "Listings: " + listings);
                 adapter = new VehicleAdapter(getContext(), listings);
                 binding.recyclerView.setAdapter(adapter);
             }
