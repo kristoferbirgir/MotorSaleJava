76486b5db3bbf94f10d56f8a4aa07ef0b54aae40
vikoand <voa6@hi.is>
2025-03-25 17:37:37 +0000

    Filters for listings implemented, still need to fix few things for it.

--- app/src/main/java/is/hbv601g/motorsale/viewModels/UserViewModel.java
+++ app/src/main/java/is/hbv601g/motorsale/viewModels/UserViewModel.java
@@ -4,11 +4,16 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
+import is.hbv601g.motorsale.DTOs.ListingDTO;
 import is.hbv601g.motorsale.DTOs.UserDTO;
 
 
 public class UserViewModel extends ViewModel {
     private final MutableLiveData<UserDTO> userLiveData = new MutableLiveData<>();
+    private final MutableLiveData<List<ListingDTO>> allListings = new MutableLiveData<>();
+    private final MutableLiveData<List<ListingDTO>> filteredListings = new MutableLiveData<>();
 
     public void setUser(UserDTO user) {
         userLiveData.setValue(user);
@@ -16,5 +21,20 @@ public class UserViewModel extends ViewModel {
 
     public LiveData<UserDTO> getUser() {
         return userLiveData;
+    }
+
+    public LiveData<List<ListingDTO>> getAllListings() {
+        return allListings;
+    }
+
+    public LiveData<List<ListingDTO>> getFilteredListings() {
+        return filteredListings;
+    }
+    public void setAllListings(List<ListingDTO> Listings) {
+        allListings.setValue(Listings);
+    }
+
+    public void setFilteredListings(List<ListingDTO> listings) {
+        filteredListings.setValue(listings);
     }
 }
